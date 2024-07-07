99b02add5d2f983f713ffe18bdf2255ade5980bd
“Djisu” <pfleischer2002@yahoo.co.uk>
2024-07-07 16:17:31 +0000

    securing env veriable

--- middleware/checkObjectId.ts
+++ middleware/checkObjectId.ts
@@ -3,8 +3,9 @@ import mongoose from 'mongoose'
 // middleware to check for a valid object id
 const checkObjectId = (idToCheck: string) => (req: Request, res: Response, next: NextFunction
   ) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params[idToCheck]))
+  if (!mongoose.Types.ObjectId.isValid(req.params[idToCheck])){
     return res.status(400).json({ msg: 'Invalid ID' })
+  } 
   next()
 }
 
